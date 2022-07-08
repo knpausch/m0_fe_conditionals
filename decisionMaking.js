89158b12f1ed00c4865238db9af5b6d47d5bc8ce
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-07-08 14:26:49 -0700

    Added my responses to the decisionMaking assignment

--- decisionMaking.js
+++ decisionMaking.js
@@ -34,10 +34,30 @@ if (bearChoice === 1) {
 
 
 // Questions
-// 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
-// 2. What variable has a new value assigned to it after the first if statement executes?
-// 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
-// 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
-// 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
-// 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
-// 7. What is your favorite ending?
+// 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16. Conditional is trying to
+//determine if doorChoice is equal to 1. If so, it changes variable bearClothing to "hat". If doorChoice number it anything but
+// one then it will change bearClothing to "scarf".
+// 2. What variable has a new value assigned to it after the first if statement executes? bearClothing
+// 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be? scarf
+// 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33. Conditional is trying
+//to determine if bearChoice equals 1, 2, 3 or is any other number. If it matches a number then it will make a string
+//interpolation of a sentence mixed with/without the bearClothing value. I noticed however these strings will not be displayed
+//in the console becasue it is lacking a console.log() function. Not sure if that's a trick question or if the author forgot
+//to enter those lines of code.
+// 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be? It will only display "You enter a dark room with two doors. Do you go through #1 or #2?
+//You see a bear putting on a hat
+//It looks like that hat is too small for the bear, do you...
+//1. Offer your own to the bear?
+//2. Point it out to the bear?
+//3. Make a dash for the next room?
+//I'm assuming if you wanted the choices to print after utilizing console.log(), then the "output" would be "You run as fast
+//as you can into the next room. It's full of snakes!";
+// 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome
+//be? You see a bear putting on a hat
+//It looks like that hat is too small for the bear, do you...
+//1. Offer your own to the bear?
+//2. Point it out to the bear?
+//3. Make a dash for the next room?
+//Again, assumming the author included console.log() then the finall line printed would be "You tell the bear the
+// hat is too small and it starts to cry!"
+// 7. What is your favorite ending? You stay with the bear and become its best friend
